cd5609ce90189f4c50731102b70e6f2c6962e38b
Arian Botine <arian.botine@shift.com.br>
2024-04-13 22:33:08 -0300

    Implementado o consumo da fila preco atraves de um servico utilizando nodejs

--- consumer-price-nodejs/consumer.js
+++ consumer-price-nodejs/consumer.js
@@ -1,2 +1,25 @@
-const amqp = require('amqplib')
+const amqp = require("amqplib");
+const queue = "PRICE";
 
+amqp
+  .connect({
+    host: "localhost",
+    port: 5672,
+    username: "admin",
+    password: 123456,
+  })
+  .then((connection) => {
+    connection
+      .createChannel()
+      .then((channel) => {
+        channel.consume(
+          queue,
+          (message) => {
+            console.log(message.content.toString());
+          },
+          { noAck: true }
+        );
+      })
+      .catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
